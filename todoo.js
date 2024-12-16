f1fa09ccefa5475c989f0a7a67c7a1702090ef14
Your Namebasit <basitali.vision@gmail.com>
2024-12-17 01:53:41 +0500

    done

---
+++ todoo.js
@@ -0,0 +1,15 @@
+
+
+let str1 = prompt("ADD YOUR FULL NAME ?");
+let str2 = prompt("ADD YOUR FAVIOURTE FRUIT ?");
+let str3 = prompt("ADD YOUR FAVIOURTE COLOR ?");
+let str4 = prompt("ADD YOUR FAVIOURTE CITY ?");
+
+let todolist = ["TO DO LIST"];
+let simple = str1 + " " + str2 + " " + str3 + " " + str4;
+
+todolist.length
+todolist.push(simple);
+console.log(todolist);
+
+
