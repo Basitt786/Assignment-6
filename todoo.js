0b81981016c55eb33ba83a6eb787f379def92c2e
Syed Basit Ali <Basitali.vision@gmail.com>
2024-12-17 20:53:18 +0500

    Update todoo.js

--- todoo.js
+++ todoo.js
@@ -6,7 +6,7 @@ let str3 = prompt("ADD YOUR FAVIOURTE COLOR ?");
 let str4 = prompt("ADD YOUR FAVIOURTE CITY ?");
 
 let todolist = ["TO DO LIST"];
-let simple = str1 + " " + str2 + " " + str3 + " " + str4;
+let simple = (str1,str2,str3);
 
 todolist.length
 todolist.push(simple);
